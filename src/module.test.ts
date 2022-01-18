9ae7660ccf0f2ec9c3ccba6bc7e4b90e019095b1
ryunosuke0922 <ryumaeda0922@gmail.com>
2022-01-18 23:18:44 +0900

    fix

--- src/module.test.ts
+++ src/module.test.ts
@@ -1,7 +1,13 @@
-import { get100FamousMountainsInJapan } from "./module";
+import { get100FamousMountainsInJapan, get100FamousMountainsInJapan_kansai } from "./module";
 
 test("IamExported returns greeting", () => {
   expect(get100FamousMountainsInJapan().find((d) => d.no === 1)?.name).toBe(
     "利尻岳"
+  );
+});
+
+test("IamExported returns greeting", () => {
+  expect(get100FamousMountainsInJapan_kansai().find((d) => d.no === 4)?.name).toBe(
+    "大峰山(八経ヶ岳)"
   );
 });
