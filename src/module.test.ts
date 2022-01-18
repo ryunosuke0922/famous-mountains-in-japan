4223b8dba94eaab1d53d39f90014faf59edac86b
ryunosuke0922 <ryumaeda0922@gmail.com>
2022-01-18 23:38:15 +0900

    fix

--- src/module.test.ts
+++ src/module.test.ts
@@ -1,4 +1,9 @@
-import { get100FamousMountainsInJapan, get100FamousMountainsInJapan_kansai } from "./module";
+import {
+  get100FamousMountainsInJapan, 
+  get100FamousMountainsInJapan_hokkaido,
+  get100FamousMountainsInJapan_kansai,
+  get100FamousMountainsInJapan_elevation,
+ } from "./module";
 
 test("IamExported returns greeting", () => {
   expect(get100FamousMountainsInJapan().find((d) => d.no === 1)?.name).toBe(
@@ -7,7 +12,20 @@ test("IamExported returns greeting", () => {
 });
 
 test("IamExported returns greeting", () => {
+  expect(get100FamousMountainsInJapan_hokkaido().find((d) => d.no === 1)?.name).toBe(
+    "利尻岳"
+  );
+});
+
+test("IamExported returns greeting", () => {
   expect(get100FamousMountainsInJapan_kansai().find((d) => d.no === 4)?.name).toBe(
+    "大峰山(八経ヶ岳)"
+  );
+});
+
+
+test("IamExported returns greeting", () => {
+  expect(get100FamousMountainsInJapan_elevation().find((d) => d.no === 4)?.name).toBe(
     "大峰山(八経ヶ岳)"
   );
 });
